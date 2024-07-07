1c17e82e62f87478e2bcaa2b11154abd900ea57c
Siddhant Patil <111656737+1543siddhant@users.noreply.github.com>
2024-07-07 18:14:01 +0530

    Model updated main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
 #Tensorflow Model Prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    model = tf.keras.models.load_model("tflite_quant_model.tflite")
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) #convert single image to batch
